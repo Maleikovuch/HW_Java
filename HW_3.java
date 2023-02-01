4e49aeba1a267e0514527c35142cd0f262631293
«maleikovuch» <maleikovuch@mail.ru>
2023-02-01 22:59:56 +0300

    Work_3

--- HW_3.java
+++ HW_3.java
@@ -3,24 +3,31 @@ import java.util.*;
 public class HW_3 
 {
 
-public static void main(String[] args)
-    {
-        Scanner con = new Scanner(System.in);
-        int n = con.nextInt();
-        int m = con.nextInt();
-        con.close();
-        int[][] dp = new int[n][m];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (i == 0 || j == 0) {
-                    dp[i][j] = 1;
-                } else {
-                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
-                }
-            }
+    public static void main(String[] args)
+        {
+            Scanner con = new Scanner(System.in);
+            int n = con.nextInt();
+            int m = con.nextInt();
+            con.close();
+            count(n,m);
         }
-    System.out.println(dp[n-1][m-1]);
-    }  
+
+        public static void count(int n, int m )
+        {
+        int[][] dp = new int[n][m];
+            for (int i = 0; i < n; i++) 
+            {
+                for (int j = 0; j < m; j++) 
+                {
+                    if (i == 0 || j == 0) {
+                        dp[i][j] = 1;
+                    } else {
+                        dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                    }
+                }  
+            }
+            System.out.println(dp[n-1][m-1]); 
+        }   
 }
     
 
